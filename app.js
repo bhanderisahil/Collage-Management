cae6df6cb7ed46162ce54ab91dede877e9fe9bae
sahilTanthetaa <sahil@tanthetaa.co.in>
2023-04-10 10:34:56 +0530

    update pakage

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 app = express();
 const port  = 7000;
-require("./config/coonection")
+// require("./config/coonection")
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://bhanderisahil:sahil%40123@cluster0.vttnglj.mongodb.net/collage",{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    console.log("coonection successful");
+}).catch((err)=>{
+    console.log("connection failed",err);
+})
+
+
+
+
+
 var cookieParser = require('cookie-parser')
 
 const adminrouter = require("./router/adminrouter")
